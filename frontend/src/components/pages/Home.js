dedbe0f643f8e8fb7ca1de1aa1cc71f5d86e63eb
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2021-08-16 02:22:59 -0700

    making the mapbox into a component

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,43 +1,15 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
- 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleGFuZHJlcG8xOTk0IiwiYSI6ImNrc2Q5bnNkMTBvbTAyem8yZm85NTMzYTAifQ.m6RJp7OLuupQtepmQ09gbw';
+import React from 'react';
+import MapComponent from '../MapComponent';
+
 
 
 const Home = () => {
-    
-
-    const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
-    });
-  });
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
-  return (
+  return(
     <div>
-    <div className="sidebar">
-    Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      <h3>Home Component</h3>
+      <MapComponent/>
     </div>
-    <div ref={mapContainer} className="map-container" />
-    </div>
-    );
+  )
 }
 
 export default Home
